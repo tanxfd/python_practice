303177e512f7d5fb27f371c06e11589aa3c16e88
tanxfdcell <759960185@qq.com>
2024-01-23 17:06:37 +0800

    关于python函数，或者说方法的用法练习

---
+++ 方法method任务/列表求和.py
@@ -0,0 +1,14 @@
+
+my_str = input("输入列表，元素之间以空格隔开")
+my_list = my_str.split(" ")
+
+
+def list_add(my_list):
+    count = 0
+    for i in range(0, len(my_list)):
+        my_list[i] = int(my_list[i])
+        count += my_list[i]
+    return count
+
+
+print(list_add(my_list))
