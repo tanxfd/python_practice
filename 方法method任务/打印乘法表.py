ceac0a41c24e2f7683d94d0b679cd1fce964aafa
tanxfdcell <759960185@qq.com>
2024-01-23 21:05:04 +0800

    关于python函数，或者说方法的用法练习

---
+++ 方法method任务/打印乘法表.py
@@ -0,0 +1,9 @@
+
+def _multiply():
+    for i in range(1, 10):
+        for j in range(1, 10 - i + 1):
+            print(repr(i), "*", repr(j), "=", repr(i * j).ljust(2), end=' ')
+        print('\n')
+
+print(_multiply())
+
