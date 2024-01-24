db6c468fcffb8bb5ad19a1cd23bd6d8a954b40fe
tanxfdcell <759960185@qq.com>
2024-01-24 13:09:17 +0800

    关于python函数，或者说方法的用法练习

--- 方法method任务/列表部分查询.py
+++ 方法method任务/列表部分查询.py
@@ -1,11 +1,11 @@
 # 列表查询
 
 def get_data(lst, num):
-    if num < len(lst):
+    if len(lst) > num >= -(len(lst)):
         return lst[num]
     else:
         return -1
 
 
 my_list = [215, 875, 415, 127]
-print(get_data(my_list, 1))
+print(get_data(my_list, -5))
